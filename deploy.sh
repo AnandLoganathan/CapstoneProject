36f550ad5fb0dbb505d7e55dd7a5d8ab8ef2656f
Ubuntu <ubuntu@ip-172-31-40-193.ec2.internal>
2024-08-21 06:49:00 +0000

    deployment changes

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 if [ -f "docker-compose.yml" ]; then
-	sudo docker-compose up -d
+	docker-compose up -d
 	if [ $? -eq 0 ]; then
 	echo "Container running successfully"
 	exit 0;
 else
 	echo "Some issue in running the container"
+	exit 2;
 	fi
 else	
 	echo "file doesn't exists"
+	exit 3;
 fi
