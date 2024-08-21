7d8c1a4c3d9490e8f73e59fd315870134a0188a9
Ubuntu <ubuntu@ip-172-31-40-193.ec2.internal>
2024-08-21 05:49:45 +0000

    Corrected script file

--- build.sh
+++ build.sh
@@ -2,11 +2,16 @@
 
 if [ -f "Dockerfile" ]; then 
 	docker build -t anandxmech/dev:capstone .
+	if [ $? -eq 0 ]; then
 	echo "Image built successfully";
+	exit 0;
+else
+	echo "Some issue in building the image";
 	exit 1;
+	fi
 else
 	echo "file doesn't exists";
-
+	exit 2;	
 fi
 
 
